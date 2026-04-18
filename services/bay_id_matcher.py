7838d6697aef2c7fa1c6884f2534cde13ff8115b
Ijat <mytimelabyrinth@gmail.com>
2026-04-18 11:27:07 +0800

    cleanup: remove dead services (bay_id_matcher, load_profile_service, substation_sync, sld_parser)

--- services/bay_id_matcher.py
+++
@@ -1,5 +0,0 @@
-# V1 SERVICE - DISABLED FOR V2
-# This service matched load profile data to Transformer/IncomingBay models
-# which no longer exist in V2 schema.
-# 
-# Future: Create NetworkLoadMatcher for matching snapshot loads to TopologyBus.
